2add88bf578331fc56f5b8700b3320f3e526ad08
Oleona <Ole-onna@mail.ru>
2022-01-16 18:18:08 +0300

    in work

--- TestForCadwise/TestParserAsync.cs
+++ TestForCadwise/TestParserAsync.cs
@@ -16,15 +16,21 @@ namespace TestForCadwise
         public struct Chunk
         {
             public int i;
-            public char[] chars;
+            //public char[] chars;
+            public List<string> fragmentText;
 
-            public Chunk(int i, char[] chars)
+            //public Chunk(int i, char[] chars)
+            //{
+            //    this.i = i;
+            //    this.chars = chars;
+
+            //}
+
+            public Chunk(int i, List<string> fragmentText)
             {
                 this.i = i;
-                this.chars = chars;
-
+                this.fragmentText = fragmentText;
             }
-
 
 
         }
@@ -45,95 +51,158 @@ namespace TestForCadwise
              }*/
 
             //var text = SplitFileChar(inputFile, BlockSize);
-            var text = SplitFileListString(inputFile, BlockSize);
 
-            for (int i = 0;i< text.Count; i++)
+            //var text = SplitFileListString(inputFile, BlockSize);
+
+            var text = SplitFileListChunk(inputFile, BlockSize);
+
+            for (int i = 0; i < text.Count; i++)
             {
-                for (int j = 0; j < text[i].Count; j++)
+                for (int j = 0; j < text[i].fragmentText.Count; j++)
                 {
-                    Console.WriteLine(text[i][j]);
+                    
+                        Console.WriteLine(text[i].fragmentText[j]);
+       
                 }
 
+                Console.WriteLine($"Закончилась структура{i}") ;
+
             }
+
+
+            //Это блок если вызываем преобразование в лист строк и его в структуры
+            //for (int i = 0; i < text.Count; i++)
+            //{
+            //    Chunk chunk = new Chunk(i, text[i]);
+            //    chunkList.Add(chunk);
+            //}
 
             Console.Write("test");
-        }
-
-
-
-
-        List<char[]> SplitFileChar(FileInfo inputFile, int BlockSize)
-        {
-            var result = new List<char[]>();
-
-            var fileLen = new FileInfo(inputFile.FullName).Length;
-
-            var totalRead = 0;
-            using (var sr = new StreamReader(inputFile.FullName))
-            {
-                while (totalRead < fileLen)
-                {
-
-                    var part = new char[BlockSize];
-                    var a = sr.Read(part, 0, part.Length);
-                    if (a == 0)
-                    {
-                        break;
-                    }
-
-                    totalRead += BlockSize;
-
-                    result.Add(part);
-                }
             }
 
-            return result;
-        }
 
 
-        List<List<string>> SplitFileListString(FileInfo inputFile, int BlockSize)
-        {
-            //var result = new List<char[]>();
-            List<List<string>> result = new List<List<string>>();
 
-            var fileLen = new FileInfo(inputFile.FullName).Length;
+            //List<char[]> SplitFileChar(FileInfo inputFile, int BlockSize)
+            //{
+            //    var result = new List<char[]>();
 
-            var totalRead = 0;
-            using (var sr = new StreamReader(inputFile.FullName))
+            //    var fileLen = new FileInfo(inputFile.FullName).Length;
+
+            //    var totalRead = 0;
+            //    using (var sr = new StreamReader(inputFile.FullName))
+            //    {
+            //        while (totalRead < fileLen)
+            //        {
+
+            //            var part = new char[BlockSize];
+            //            var a = sr.Read(part, 0, part.Length);
+            //            if (a == 0)
+            //            {
+            //                break;
+            //            }
+
+            //            totalRead += BlockSize;
+
+            //            result.Add(part);
+            //        }
+            //    }
+
+            //    return result;
+            //}
+
+
+            List<List<string>> SplitFileListString(FileInfo inputFile, int BlockSize)
             {
-                while (totalRead < fileLen)
 
+                List<List<string>> result = new List<List<string>>();
+
+                var fileLen = new FileInfo(inputFile.FullName).Length;
+
+                var totalRead = 0;
+                using (var sr = new StreamReader(inputFile.FullName))
                 {
-                    var part = new List<string>();
-                    //var a = sr.Read(part, 0, part.Length);
-
-                    for (int i = 0; i < 10; i++)
+                    while (totalRead < fileLen)
 
                     {
-                        string temp = sr.ReadLine();
-                        if (String.IsNullOrEmpty(temp))
+                        var part = new List<string>();
+
+                        for (int i = 0; i < 10; i++)
+
                         {
-                            //Пытаюсь сделать чтобы в каждую секцию писалось 10 строк но тогда в конце уходим в вечный цикл так как I все время
-                            //вычитается. wile никак так как в последнем блоке не 10 строк а меньше
-                            // i--;
-                            // breac  не пойдет так как в тексте есть пустые строки
-                            continue;
+                            string temp = sr.ReadLine();
+                            if (String.IsNullOrEmpty(temp))
+                            {
+                                //Пытаюсь сделать чтобы в каждую секцию писалось 10 строк но тогда в конце уходим
+                                //в вечный цикл так как I все время
+                                //вычитается. wile никак так как в последнем блоке не 10 строк а меньше
+                                // i--;
+                                //Предполагаю что можно пренебресь так как размеры блоков будут большие
+                                //и разница в несколько строк не важна
+                                continue;
+                            }
+                            part.Add(temp);
                         }
-                        part.Add(temp);
-                    }
 
-                    totalRead += BlockSize;
+                        totalRead += BlockSize;
 
-                    if (part.Count != 0)
-                    {
-                        result.Add(part);
+                        if (part.Count != 0)
+                        {
+                            result.Add(part);
+                        }
                     }
                 }
+
+                return result;
             }
 
-            return result;
+
+            List<Chunk> SplitFileListChunk(FileInfo inputFile, int BlockSize)
+            {
+
+                List<Chunk> result = new List<Chunk>();
+
+                var fileLen = new FileInfo(inputFile.FullName).Length;
+                int counter = 0;
+                var totalRead = 0;
+                using (var sr = new StreamReader(inputFile.FullName))
+                {
+                    while (totalRead < fileLen)
+
+                    {
+                        var part = new List<string>();
+
+                        for (int i = 0; i < 10; i++)
+
+                        {
+                            string temp = sr.ReadLine();
+                            if (String.IsNullOrEmpty(temp))
+                            {
+                                //Пытаюсь сделать чтобы в каждую секцию писалось 10 строк но тогда в конце уходим
+                                //в вечный цикл так как I все время
+                                //вычитается. wile никак так как в последнем блоке не 10 строк а меньше
+                                // i--;
+                                //Предполагаю что можно пренебресь так как размеры блоков будут большие
+                                //и разница в несколько строк не важна
+                                continue;
+                            }
+                            part.Add(temp);
+                        }
+
+                        totalRead += BlockSize;
+
+
+                        if (part.Count != 0)
+                        {
+                            Chunk chunk = new Chunk(counter, part);
+                            result.Add(chunk);
+                        }
+                    }
+                }
+
+                return result;
+            }
+
         }
-
-
     }
-}
+
